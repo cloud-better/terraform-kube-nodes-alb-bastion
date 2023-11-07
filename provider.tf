23ff6f6c6b26310ae0a93bcc64e0d602e175488e
CloudBetterGit <150063402+cloudbettergit@users.noreply.github.com>
2023-11-07 20:21:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "68d12938-6cd4-4f41-a3f3-5818261601e4"
-  tenant_id = "57135b04-f0d0-4240-af74-1c11947a9f26"
-  client_id = "c635ab76-70eb-4a2f-bea8-e68cdfcfcafe"
-  client_secret = "1AE8Q~DDBfQ9HW5pQ2eJY0hwOoDk.-dgEoWZwdrp"
+  subscription_id = "68d12938-*******"
+  tenant_id = "57135b04-**********"
+  client_id = "c635ab76-**********"
+  client_secret = "1AE8Q~**********"
   skip_provider_registration = true
   features {}  
 }
